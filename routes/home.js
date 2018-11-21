f9522540aeee5686549c1d6b5963a7cb620ad097
Alan Alvarez Sánchez <alan@MacBook-Air-de-Alan.local>
2018-11-21 11:52:04 -0600

    Esqueleto1 termiando, paginas: home, addcall, explore.

--- routes/home.js
+++ routes/home.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Convocatorias' });
+  res.render('home', { title: 'Home' });
 });
 
 router.get('/adduser', function (req, res){
     res.render('adduser', {title: 'Crear Nuevo Usuario'});
+});
+
+router.get('/addcall', function(req, res){
+  res.render('addcall', {title:'Crea una nueva convocatoria'});
+});
+
+router.get('/explore', function(req, res){
+  res.render('explore', {title:'Explorar'});
 });
 
 module.exports = router;
