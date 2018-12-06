b818e2dfac509adf285819687eb8598f2aad1a2e
AlanAlvarez5 <aa.alvarezsanchez@ugto.mx>
2018-12-06 04:28:50 -0600

    Acomodo de la navegación

---
+++ public/scripts/sendeddocs.js
@@ -0,0 +1,8 @@
+function docsenviados() {
+    alert("¡Documentos enviados!");
+    window.location.href = "http://localhost:3000/explore";
+};
+
+function uploaddoc() {
+    window.location.href = "http://localhost:3000/uploaddoc";
+};
